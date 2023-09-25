d3dec18a1678c915ae67afb4ccb8a11d72b3335f
Nirbhay Agarwal <98315109+Nirbhay2k1@users.noreply.github.com>
2023-09-25 16:59:57 +0530

    Update and rename Test 3 to Test 3.py

---
+++ Test 3.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return "Cannot pop from an empty stack."
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return "Empty stack."
+s=Stack()
+s.push(5)
+s.push(4)
+print("Stack size:", s.size())
