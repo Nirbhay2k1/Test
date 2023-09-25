9a4411228981dfac3a76b1a59e39dee98eba6ef5
Nirbhay Agarwal <98315109+Nirbhay2k1@users.noreply.github.com>
2023-09-25 17:16:13 +0530

    Update Test 5.py

--- Test 5.py
+++ Test 5.py
@@ -1,26 +1,21 @@
 class Queue:
     def __init__(self):
         self.items = []
+
     def enqueue(self, item):
         self.items.append(item)
+
     def dequeue(self):
         if not self.is_empty():
             return self.items.pop(0)
-        else:
-            raise IndexError("Cannot dequeue from an empty queue.")
+        return ("Cannot dequeue from an empty queue.")
+
     def is_empty(self):
         return len(self.items) == 0
 
-queue = Queue()
-queue.enqueue(10)
-queue.enqueue(20)
-queue.enqueue(30)
-queue.enqueue(40)
-queue.enqueue(50)
-
-print("Current Queue:", queue.items)
-dequeued_item = queue.dequeue()
-print("Dequeued item:", dequeued_item)
-dequeued_item = queue.dequeue()
-print("Dequeued item:", dequeued_item)
-print("Updated Queue:", queue.items)
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+print(q.items)
