076da1d80b6ce5d8e151555477929581302efda5
Nirbhay Agarwal <98315109+Nirbhay2k1@users.noreply.github.com>
2023-09-25 16:41:40 +0530

    Create Test 2.py

---
+++ Test 2.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    def subtract(self,a,b):
+        return a-b
+    def multiply(self,a,b):
+        return a*b
+    def divide(self,a,b):
+        if b!=0:
+            return a/b;
+        return ("Cannot divide by zero.")
+      
+calc=Calculator()
+result=(calc.add(2,3))
+print("Addition =",result)
+
+   
